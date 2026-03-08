3c64886383ea2b44dfcecc6d4fb5ffd4fc9ba011
sharayu warkhede <sharayuwarkhede@gmail.com>
2026-03-08 23:50:10 +0530

    Add files via upload

---
+++ Codeforces/AgainTwentyFive.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AgainTwentyFive {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        if(n==0){
+            System.out.println("1");
+        }
+        else{
+             System.out.println("25");
+        } 
+    } 
+}
